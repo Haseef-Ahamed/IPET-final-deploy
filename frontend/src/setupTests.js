e34979229612ac60d0460f6b2557eb3d23c83eb4
Haseef-Ahamed <mshaseefat@gmail.com>
2025-11-11 18:42:14 +0530

    Demo Storage for testion Solve the buggs1

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,44 +1,36 @@
-const globalScope = typeof globalThis !== "undefined"
-  ? globalThis
-  : typeof window !== "undefined"
-  ? window
-  : typeof global !== "undefined"
-  ? global
-  : {};
+const globalScope =
+  typeof globalThis !== "undefined"
+    ? globalThis
+    : typeof window !== "undefined"
+    ? window
+    : typeof global !== "undefined"
+    ? global
+    : {};
 
-if (!globalScope.localStorage) {
-  globalScope.localStorage = {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
-    clear: () => {},
-  };
-}
-if (!globalScope.sessionStorage) {
-  globalScope.sessionStorage = {
-    getItem: () => null,
-    setItem: () => {},
-    removeItem: () => {},
-    clear: () => {},
-  };
-}
-if (!globalScope.IntersectionObserver) {
-  globalScope.IntersectionObserver = class {
-    constructor() {}
-    observe() {}
-    disconnect() {}
-    unobserve() {}
-    takeRecords() { return []; }
-  };
-}
-
-if (!globalScope.scrollTo) {
-  globalScope.scrollTo = () => {};
-}
+// FORCE overwrite, not just define!
+globalScope.localStorage = {
+  getItem: () => null,
+  setItem: () => {},
+  removeItem: () => {},
+  clear: () => {}
+};
+globalScope.sessionStorage = {
+  getItem: () => null,
+  setItem: () => {},
+  removeItem: () => {},
+  clear: () => {}
+};
+globalScope.IntersectionObserver = class {
+  constructor() {}
+  observe() {}
+  disconnect() {}
+  unobserve() {}
+  takeRecords() { return []; }
+};
+globalScope.scrollTo = () => {};
 if (
   globalScope.document &&
-  globalScope.document.documentElement &&
-  !globalScope.document.documentElement.scrollTo
+  globalScope.document.documentElement
 ) {
   globalScope.document.documentElement.scrollTo = () => {};
 }
